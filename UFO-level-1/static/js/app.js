1c86c193a60eb072b2fb26ae29b85a4ddb8d21cf
das <don.a.sarge@gmail.com>
2021-04-11 18:22:19 -0400

    ufo-1 commit

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,46 @@
+// from data.js
+var tableData = data;
+
+
+//getting reference of table's body
+var ufoTable = document.getElementById("ufo-table-body");
+var rows = "";
+
+//creating the table rows with the data
+tableData.map((element) => {
+  rows += "<tr>" +
+    "<td>" + element.datetime + "</td>" +
+    "<td>" + element.city + "</td>" +
+    "<td>" + element.state + "</td>" +
+    "<td>" + element.country + "</td>" +
+    "<td>" + element.shape + "</td>" +
+    "<td>" + element.durationMinutes + "</td>" +
+    "<td>" + element.comments + "</td>" +
+  "</tr>";
+});
+
+//filling the table
+  //getting table input reference
+ufoTable.innerHTML = rows;
+
+function onChangeDateFilter(event) {
+
+  var dateInput = document.getElementById("datetime");
+  rows = "";
+  
+  //filter table rows by the date input value
+  tableData.filter((row) => (row.datetime === dateInput.value)).map((element) => {
+    rows += "<tr>" +
+      "<td>" + element.datetime + "</td>" +
+      "<td>" + element.city + "</td>" +
+      "<td>" + element.state + "</td>" +
+      "<td>" + element.country + "</td>" +
+      "<td>" + element.shape + "</td>" +
+      "<td>" + element.durationMinutes + "</td>" +
+      "<td>" + element.comments + "</td>" +
+      "</tr>";
+  });
+  
+  //fill the table again
+  ufoTable.innerHTML = rows;
+}
